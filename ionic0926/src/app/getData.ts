3aa7ebf9f41a2675ebe44e0ee2022a835e0c84b8
WULIbinbin™ <WULIbinbin@users.noreply.github.com>
2017-12-19 18:14:04 +0800

    Delete getData.ts

--- ionic0926/src/app/getData.ts
+++
@@ -1,7 +0,0 @@
-import AV from '_leancloud-storage@3.2.1@leancloud-storage/dist/av-min.js'
-
-const appId = 'GODTeoChmcJVgC8vdQeBop4i-gzGzoHsz';
-const appKey = 'HrHNKY2Sf77gWgN5gBFal1nM';
-AV.init({ appId, appKey });
-
-export default AV; 
